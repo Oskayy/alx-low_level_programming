e53b36c04f8f15d9414462dad0b69ec251ff87b5
OSmond <osmondsedem@gmail.com>
2022-12-13 14:35:09 -0800

    correction to task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-
 /**
- * main - prints _putchar and a new line
- * Return: Always 0
+ * main - print _putchar
+ * Return: 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,3 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+	return (0);
+}
