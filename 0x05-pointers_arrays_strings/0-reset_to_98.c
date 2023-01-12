15cccd5491921fcbd76652528955d9c2d77e2cdf
OSmond <osmondsedem@gmail.com>
2023-01-12 08:25:47 -0800

    tsk 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * main - checks the code
  * Return - Always 0
- * @n - input value
+ * @n: input value
  */
 
 void reset_to_98(int *n);
@@ -14,6 +14,7 @@ void reset_to_98(int *n);
 int main(void)
 {
 	int n = 402;
+
 	reset_to_98(&n);
 	_putchar(n);
 	_putchar('\n');
