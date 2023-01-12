0c979e98b4f7baa5d01fa9ebca2df29b131f84e2
OSmond <osmondsedem@gmail.com>
2023-01-12 08:52:47 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,20 +2,11 @@
 
 /**
  * main - checks the code
- * Return - Always 0
+ * Return - n
  * @n: input value
  */
 
 void reset_to_98(int *n);
 {
 	*n = 98;
-}
-
-int main(void)
-{
-	int n = 402;
-
-	reset_to_98(&n);
-	_putchar(n);
-	_putchar('\n');
 }
