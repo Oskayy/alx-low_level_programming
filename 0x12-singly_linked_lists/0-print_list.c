e61c0f45c0703b0d718a04e1fda1136e47282750
Osmond <osmondsedem.com>
2023-01-26 22:25:17 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,11 +3,10 @@
 
 /**
  * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * @h: pointer to the list_t
  *
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
